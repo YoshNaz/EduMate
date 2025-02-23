3f26fea4522965a92b51aae1b4d5ea65ca8b8cb6
YoshNaz <138871031+YoshNaz@users.noreply.github.com>
2025-02-23 20:11:00 +0800

    Database and some scripting (confirmation)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, send_file
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from io import BytesIO
 
 app=Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -8,15 +9,21 @@ db = SQLAlchemy(app)
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
+    filename = db.Column(db.String(50))
+    data = db.Column(db.LargeBinary)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
-@app.route('/')
-def indedx():
+@app.route('/',methods=['GET','POST'])
+def index():
+    if request.method == "POST":
+        file = request.files["file"]
+        upload = Todo(filename=file.filename, data=file.read())
+        db.session.add(upload)
+        db.session.commit()
+        return render_template('index.html')
     return render_template('index.html')
 
 if __name__ == "__main__":
