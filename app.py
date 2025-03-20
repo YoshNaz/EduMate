dd7606721cab4a134b6a46bc68aa0688d57ff207
Athirah Husna <126548306+thirahusnaa@users.noreply.github.com>
2025-03-21 00:27:03 +0800

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from summarizer import summarize
